270fdccb2de61f3b87f65f30e505c82cafc45477
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2024-02-11 12:52:39 +0530

    Update binary search.c

--- binary search.c
+++ binary search.c
@@ -1,43 +1,50 @@
 #include <stdio.h>
-int binary(int a[],int beg,int end,int val){
-    int mid;
-    if(end>-beg){
-        mid = (end+beg)/2;
+
+int binary(int a[], int beg, int end, int val) {
+    if (beg <= end) {
+        int mid = beg + (end - beg) / 2; // Calculate mid index
+        if (a[mid] == val) {
+            return mid + 1;
+        } else if (a[mid] < val) {
+            return binary(a, mid + 1, end, val);
+        } else {
+            return binary(a, beg, mid - 1, val);
+        }
     }
-    if(a[mid]==val){
-        return mid+1;
-    }
-    else if(a[mid]<val){
-        return binary(a,mid+1,end,val);
-    }
-    else{
-        return binary(a,beg,mid+1,val);
-    }
-    return -1;
+    return -1; // Element not found
 }
 
-int main(){
-    int a[9];
+int main() {
+    int a[5]; // Change size to 5
     printf("Enter 5 integers: ");
-    for(int i = 0; i < 6; ++i) {
-     scanf("%d ", &a[i]);
+    for (int i = 0; i < 5; ++i) {
+        scanf("%d", &a[i]);
     }
     int val;
     printf("Enter the value need to be searched: ");
-    scanf("%d ",&val);
-    int n = sizeof(a)/(sizeof(a[0]));
-    int res = binary(a,0,n-1,val);
+    scanf("%d", &val);
+    int n = sizeof(a) / sizeof(a[0]);
+    int res = binary(a, 0, n - 1, val);
     printf("Elements: ");
-    for(int i=0;i<n;i++){
-        printf("%d ",a[i]);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
     }
-    printf("Elements searched %d:\n",val);
-    if (res== -1){
-        printf("Element is not found");
-    }else{
-        printf("Elements found at %d\n",res);
+    printf("\nElement searched: %d\n", val);
+    if (res == -1) {
+        printf("Element is not found\n");
+    } else {
+        printf("Element found at position: %d\n", res);
     }
 
     return 0;
 }
 
+
+*/
+Enter 5 integers: 4 5 8 1 7
+Enter the value need to be searched: 4
+Elements: 4 5 8 1 7 
+Element searched: 4
+Element found at position: 1
+
+*/
