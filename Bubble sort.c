280b19ae4b28a9fad60225177cb17cb3d3b4652c
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2024-04-05 00:00:04 +0530

    Update Bubble sort.c

--- Bubble sort.c
+++ Bubble sort.c
@@ -1,38 +1,41 @@
 #include<stdio.h>
-void print(int a[],int n){
-    int i;
-    for(i=0;i<n;i++){
-        printf("%d\t",a[i]);
+
+void print(int a[], int n){
+    for(int i = 0; i < n; i++){
+        printf("%d\t", a[i]);
     }
 }
-void bubble(int a[],int n){
-    int i,j,temp;
-    for(i=0;i<n;i++){
-        for(j=i+1;j<n;j++){
-            if(a[j]<a[i]){
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
+
+void bubble(int a[], int n){
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(a[j] < a[i]){
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
             }
         }
     }
 }
 
-void main(){
-    int i,j,temp;
-    int a[5]={10,65,23,7,57};
-    int b[5]={10,65,23,7,57};
-    int n = sizeof(a)/sizeof(a[0]);
-    int m = sizeof(b)/sizeof(b[0]);
+int main(){
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+    printf("Enter %d elements: ", n);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+
     printf("Before sorting array of elements\n");
-    print(a,n);
-    bubble(a,n);
-    printf("\nAfter sorting array of elements  are\n");
-    print(a,n);
+    print(a, n);
+
+    bubble(a, n);
+
+    printf("\nAfter sorting array of elements\n");
+    print(a, n);
+
+    return 0;
 }
-
-
-/* Before sorting array of elements
-10	65	23	7	57	
-After sorting array of elements  are
-7	10	23	57	65	*/
